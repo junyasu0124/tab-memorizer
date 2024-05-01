c5b8851d9d51ea485968ecd2a3d6266754e8a36b
junyasu <jumpeiyasue@gmail.com>
2024-05-02 03:28:39 +0900

    feat: Added code to display the history of the current tab in a popup

--- src/popup/script.ts
+++ src/popup/script.ts
@@ -1,10 +1,16 @@
 document.addEventListener("DOMContentLoaded", async () => {
-  const tabs = await chrome.tabs.query({ active: true, currentWindow: true });
+  const saveCurrentTabElement = document.getElementById('saveCurrentTab');
+  if (saveCurrentTabElement)
+    saveCurrentTabElement.onclick = async () => await saveCurrentTab();
 
+  const clearSavedElement = document.getElementById('clearSaved');
+  if (clearSavedElement)
+    clearSavedElement.onclick = async () => await clearSaved();
+
+  const tabs = await chrome.tabs.query({ active: true, currentWindow: true });
   if (tabs.length === 0) return;
 
   const activeTab = tabs[0];
-
   if (!activeTab.id) return;
 
   const tabHistory = await getTabHistory(activeTab.id);
@@ -17,10 +23,66 @@ document.addEventListener("DOMContentLoaded", async () => {
 });
 
 async function getTabHistory(tabId: number): Promise<{ url: string | undefined, title: string | undefined }[]> {
-  const keyInLocalStorage = 'tab_' + tabId;
+  const keyInLocalStorage = 'pre_tab_' + tabId;
   const savedTabInfo = await chrome.storage.local.get(keyInLocalStorage);
   if (savedTabInfo)
     return savedTabInfo[keyInLocalStorage] || [];
   else
     return [];
+}
+
+async function saveGroup(id: string, group: { name: string, saved_date: string, tabs: { history: { url: string | undefined, title: string | undefined }[] }[] }) {
+  await chrome.storage.local.set({ [id]: group });
+  const savedValue = await chrome.storage.local.get('saved');
+  const saved: string[] = savedValue['saved'] || [];
+  saved.push(id);
+  await chrome.storage.local.set({ ['saved']: saved });
+}
+
+async function isInLocalStorage(key: string) {
+  const value = await chrome.storage.local.get(key);
+  return !!value[key];
+}
+
+async function saveCurrentTab() {
+  const tabs = await chrome.tabs.query({ active: true, currentWindow: true });
+  if (tabs.length === 0) return;
+
+  const currentTab = tabs[0];
+  if (!currentTab.id) return;
+
+  const tabHistory = await getTabHistory(currentTab.id);
+
+  let id = 'saved_tab_' + currentTab.id;
+  if (await isInLocalStorage(id)) {
+    for (let i = 2; i < 1000; i++) {
+      id = 'saved_tab_' + currentTab.id + '_' + i;
+      if (!await isInLocalStorage(id)) break;
+      if (i === 99) return;
+    }
+  }
+
+  const data = {
+    name: currentTab.title || '',
+    saved_date: convertDateToString(new Date()),
+    tabs: [{ history: tabHistory }]
+  };
+  await saveGroup(id, data);
+}
+
+async function clearSaved() {
+  const savedValue = await chrome.storage.local.get('saved');
+  const saved: string[] = savedValue['saved'] || [];
+  saved.forEach(async (id) => {
+    await chrome.storage.local.remove(id);
+  });
+  await chrome.storage.local.remove('saved');
+}
+
+function convertDateToString(date: Date) {
+  return `${date.getFullYear()}-${zeroPadding(date.getMonth() + 1, 2)}-${zeroPadding(date.getDate(), 2)} ${zeroPadding(date.getHours(), 2)}:${zeroPadding(date.getMinutes(), 2)}:${zeroPadding(date.getSeconds(), 2)}`;
+
+  function zeroPadding(num: number, length: number) {
+    return ('0000000000' + num).slice(-length);
+  }
 }
